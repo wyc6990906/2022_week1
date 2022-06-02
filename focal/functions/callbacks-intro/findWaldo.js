f77aaa312fa382e02468552c20fa95d705267c82
Yichuan wang <44794108+wyc6990906@users.noreply.github.com>
2022-06-02 11:15:37 -0600

    function

---
+++ focal/functions/callbacks-intro/findWaldo.js
@@ -0,0 +1,20 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function (names, found) {
+  // for (let i = 0; i < names.length; i++) {
+  //   let name = names[i];
+  //   if (name === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
+  names.forEach((item, index) => {
+    if (item === "Waldo") {
+      found(index)
+    }
+  })
+}
+
+const actionWhenFound = function (index) {
+  console.log(`Found Waldo at index ${index}!`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
